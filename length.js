476afa43415f1afa719ac9f4de6123345ec8747c
Your Name <you@example.com>
2024-10-16 14:23:40 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,46 @@
+const number = 20;
+const x = 40;
+var y = 30;
+let p = 100;
+
+if (number < 18) {
+  console.log("Good day");
+}
+else if( number<25 ){
+    console.log("Tanni");
+}
+if (x < 88) {
+    console.log("good girl");
+  }
+  else if( x<9){
+      console.log("bad girl");
+  }
+  
+  if (y < 55) {
+    console.log(" bangladesh");
+  }
+  else if( y<20){
+      console.log("bad girl");
+  }
+  if (p < 5) {
+    console.log("I Love Biriyani");
+  }
+  else if( p<200){
+      console.log("Bangladesh is a small country");
+  }
+
+  var numbers = [10, 22,33,44,55,66,77,88,99];
+
+  for(var i = 2; i<= 6; i++ ){
+      var element =numbers[i];
+      console.log(element)
+  }
+  
+  
+  
+  
+  
+  for(var Narugiven = 0; Narugiven < 10; Narugiven++){
+      console.log("tanni,mim")
+      console.log(Narugiven)
+  }
